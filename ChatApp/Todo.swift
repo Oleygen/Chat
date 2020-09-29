cae214efdb4591848a9f79d47f511228553b19df
Denis <rybkindenu@gmail.com>
2020-09-30 01:04:29 +0300

    fix ui

---
+++ ChatApp/Todo.swift
@@ -0,0 +1,24 @@
+//
+//  Todo.swift
+//  ChatApp
+//
+//  Created by Den on 2020-09-30.
+//  Copyright © 2020 Den. All rights reserved.
+//
+
+import Foundation
+
+/*
+ 
+ login
+ 
+ chat firebase logic
+ 
+ tableview cells
+ 
+ burger menu
+ 
+ 
+ check layout
+ 
+ */
