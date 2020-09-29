d9b2e4c14d74beb0185ebec8666ded581d774e28
Denis <rybkindenu@gmail.com>
2020-09-29 21:03:08 +0300

    add outlets

--- ChatApp/LoginViewController.swift
+++ ChatApp/LoginViewController.swift
@@ -8,16 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
     
     @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var createAccountButton: UIButton!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
+    
+    
+    
+    
 
 }
 
