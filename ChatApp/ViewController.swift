a7e02b3efd0b9851533e5e3908712f4e3e7d3414
Denis <rybkindenu@gmail.com>
2020-09-29 20:57:07 +0300

    init commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Den on 2020-09-29.
+//  Copyright © 2020 Den. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
