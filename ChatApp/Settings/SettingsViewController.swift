dc64d9af0149acccb8cf9d942da8d17ca8c7a1d4
Denis <rybkindenu@gmail.com>
2020-09-30 07:28:54 +0300

    add settings screen

---
+++ ChatApp/Settings/SettingsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsViewController.swift
+//  ChatApp
+//
+//  Created by Den on 2020-09-30.
+//  Copyright © 2020 Den. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
