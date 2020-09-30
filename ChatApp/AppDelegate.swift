25f0599ddbb4e9200631e425e36db7a0e0f07ed9
Denis <rybkindenu@gmail.com>
2020-09-30 11:05:57 +0300

    clean up code

--- ChatApp/AppDelegate.swift
+++ ChatApp/AppDelegate.swift
@@ -16,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
-        print("isLoggged \(Firebase.Auth.auth().currentUser)")
         return true
     }
 
